886c8823bad706e20046be952aa483d958486bc7
Wee Seng <wee.bscl@gmail.com>
2023-03-03 09:05:29 +0800

    init spring boot imports

---
+++ src/test/java/nus/iss/tfip/pafassessment/PafAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package nus.iss.tfip.pafassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PafAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
